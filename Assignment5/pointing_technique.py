5bb815d04b7010491b80fa8c9894892e418082b5
felixkalley <felix.kalley@gmail.com>
2017-06-12 22:25:25 +0200

    img as cursor

--- Assignment5/pointing_technique.py
+++ Assignment5/pointing_technique.py
@@ -5,18 +5,24 @@
 # imports
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QCursor
+from PyQt5.QtGui import QCursor, QColor
 
 
 class SpecialCursor(object):
     def __init__(self, window):
         self.givenWindow = window
-        self.givenWindow.setOverrideCursor(QCursor(Qt.WaitCursor));
+        #self.givenWindow.setOverrideCursor(QCursor(Qt.CrossCursor));
         self.cursor = QtGui.QCursor()
         self.cursor.setPos(1700, 700)
+        self.mycolor = QtGui.QColor("white")
         print(self.cursor.shape())
         self.cursor.setShape(2)
         print(self.cursor.shape())
-
+        pm = QtGui.QPixmap('test2.bmp')
+        bm = pm.createMaskFromColor(self.mycolor, Qt.MaskOutColor)
+        #pm.setAlphaChannel(bm)
+        self.cursor = QtGui.QCursor(pm)
+        self.givenWindow.setOverrideCursor(self.cursor)
+        #self.setCursor(cursor)
         #self.cursor.shape = Qt.CrossCursor()
         #self.cursor.setShape(self, Qt.CrossCursor())
