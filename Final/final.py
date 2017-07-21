c7bd183c77e315ffebffe7ef25532e3859c05bf3
LenaManschewski <lena@manschewski.de>
2017-07-21 18:52:00 +0200

    changed to QTApplication with methods and classes

--- Final/final.py
+++ Final/final.py
@@ -8,6 +8,7 @@ import pyaudio
 import sys
 import wiimote
 import time
+from PyQt5 import uic, QtWidgets, QtCore, QtGui
 
 # felix: bisherige Funktionalität - wii connecten, A-Knopf spielt kurzen Ton, oder lang bei gedrückt halten.
 # felix: +/- Knöpfe erhöhen oder senken Tonhöhe, dauert manchmal kurz.
@@ -15,102 +16,97 @@ import time
 # felix: Beim starten kommt echt viel text. ich weiß nicht was das ist... scheint vom stream zu sein.
 
 
-# ----------------------------------------
-# felix: altes wimmerbeispiel wiimote demo zum connecten
-input("Press the 'sync' button on the back of your Wiimote Plus " +
-      "or buttons (1) and (2) on your classic Wiimote.\n" +
-      "Press <return> once the Wiimote's LEDs start blinking.")
+class MusicMaker(QtWidgets.QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        # felix: das hier ist die frequenz variable, die hängt irgendwie dumm in der gegend rum.
+        # felix: ich weiß nicht, wie man das mit der Frequenzänderung anders realisieren könnte, daher erstmal so...
+        # lena: die frequenzen ändern sich pro ton nicht gleichmäßig ändert,
+        #       würde ich eine Liste an möglichen Tönen vorgeben?
+        self.myfrequency = 261
+        self.connect_wiimote()
+        self.prepareSound()
+        self.registerButtons()
 
-if len(sys.argv) == 1:
-    addr, name = wiimote.find()[0]
-elif len(sys.argv) == 2:
-    addr = sys.argv[1]
-    name = None
-elif len(sys.argv) == 3:
-    addr, name = sys.argv[1:3]
-print(("Connecting to %s (%s)" % (name, addr)))
-wm = wiimote.connect(addr, name)
-# -----------------------------------------------
+    # connect to WiiMote with given MAC-address
+    # Lena: muss noch umgebaut werden für Oberfläche!
+    def connect_wiimote(self):
+        input("Press the 'sync' button on the back of your Wiimote Plus " +
+              "or buttons (1) and (2) on your classic Wiimote.\n" +
+              "Press <return> once the Wiimote's LEDs start blinking.")
+        
+        if len(sys.argv) == 1:
+            addr, name = wiimote.find()[0]
+        elif len(sys.argv) == 2:
+            addr = sys.argv[1]
+            name = None
+        elif len(sys.argv) == 3:
+            addr, name = sys.argv[1:3]
+        print(("Connecting to %s (%s)" % (name, addr)))
+        self.wm = wiimote.connect(addr, name)
 
+    # While-Schleife um auf Button presses zu hören (muss umgebaut werden)
+    def registerButtons(self):
+        while True:
+            if self.wm.buttons["A"]:
+                self.play_tone(self.myfrequency)
+            if self.wm.buttons["Plus"]:
+                self.up_frequency()
+            if self.wm.buttons["Minus"]:
+                self.down_frequency()
+            time.sleep(0.1)
 
+        # felix: das killt scheinbar den stream und python audio.
+        # felix: stand so in dem beispiel, weiß nicht, ob wir das brauchen.
+        self.stream.close()
+        p.terminate()
+    
+    # http://milkandtang.com/blog/2013/02/16/making-noise-in-python/
+    def prepareSound(self):
+        p = pyaudio.PyAudio()
+        self.stream = p.open(format=pyaudio.paFloat32,
+                        channels=1, rate=44100, output=1)
+        
+        
+    # http://milkandtang.com/blog/2013/02/16/making-noise-in-python/
+    def sine(self, frequency, length, rate):
+        length = int(length * rate)
+        factor = float(frequency) * (math.pi * 2) / rate
+        return numpy.sin(numpy.arange(length) * factor)
 
-# ---------------------------------------------
-# http://milkandtang.com/blog/2013/02/16/making-noise-in-python/
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paFloat32,
-                    channels=1, rate=44100, output=1)
-
-
-# http://milkandtang.com/blog/2013/02/16/making-noise-in-python/
-# felix: macht scheinbar ne kurve aus frequency, length und rate
-def sine(frequency, length, rate):
-    length = int(length * rate)
-    factor = float(frequency) * (math.pi * 2) / rate
-    return numpy.sin(numpy.arange(length) * factor)
-# ----------------------------------------------
-
-
-# ------------------------------------------------
-# felix: das hier ist die frequenz variable, die hängt irgendwie dumm in der gegend rum.
-# felix: ich weiß nicht, wie man das mit der Frequenzänderung anders realisieren könnte, daher erstmal so...
-myfrequency = 261
-# -------------------------------------------------
-
-
-
-
-
-
-# -----------------------------------------------------------
-# http://milkandtang.com/blog/2013/02/16/making-noise-in-python/
-# felix: length in seconds
-# felix: lower frequency = deeper tone
-# felix: rate is standard for sound, probably better to not change
-# felix: i dont know what chunks and streams are...
-def play_tone(stream, frequency, length=0.5, rate=44100):
-    chunks = []
-    chunks.append(sine(frequency, length, rate))
-
-    chunk = numpy.concatenate(chunks) * 0.25
-
-    stream.write(chunk.astype(numpy.float32).tostring())
-# --------------------------------------------------------------
-
-
-# --------------------------------------------------------------
-# erhöht die frequenz um magic number 100
-def up_frequency():
-    global myfrequency
-    myfrequency += 100
-    print(myfrequency)
-
-# senkt die frequenz um magic number 100
-def down_frequency():
-    global myfrequency
-    myfrequency -= 100
-    print(myfrequency)
-# --------------------------------------------------------------
+    # http://milkandtang.com/blog/2013/02/16/making-noise-in-python/
+    # length in seconds
+    # lower frequency = deeper tone
+    # standard rate is 44.1kHz
+    # felix: i dont know what chunks and streams are...
+    def play_tone(self, frequency, length=1, rate=44100):
+        chunks = []
+        chunks.append(self.sine(frequency, length, rate) / 2)
+    
+        chunk = numpy.concatenate(chunks) * 0.25
+    
+        self.stream.write(chunk.astype(numpy.float32).tostring())
 
 
+    # erhöht die frequenz um magic number 100
+    def up_frequency(self):
+        self.myfrequency += 100
+        print(self.myfrequency)
+    
+    # senkt die frequenz um magic number 100
+    def down_frequency(self):
+        self.myfrequency -= 100
+        print(self.myfrequency)
+    
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    musicMaker = MusicMaker()
+    sys.exit(app.exec_())
 
 
-# -------------------------------------------------
-# felix: wimmer button zeugs
-while True:
-    if wm.buttons["A"]:
-        play_tone(stream, myfrequency)
-    if wm.buttons["Plus"]:
-        up_frequency()
-    if wm.buttons["Minus"]:
-        down_frequency()
-    time.sleep(0.1)
-#-------------------------------------------------
+if __name__ == '__main__':
+    main()
 
 
-
-
-# ---------------------------------------------
-# felix: das killt scheinbar den stream und python audio.
-# felix: stand so in dem beispiel, weiß nicht, ob wir das brauchen.
-stream.close()
-p.terminate()
